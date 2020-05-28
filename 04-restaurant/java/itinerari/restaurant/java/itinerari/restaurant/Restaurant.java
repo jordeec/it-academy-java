c9a9baed09f69d77520b75b7f3b3a12be22d08a3
jordeec <61867781+jordeec@users.noreply.github.com>
2020-05-28 14:38:38 +0200

    Pujada inicial restaurant

---
+++ 04-restaurant/java/itinerari/restaurant/java/itinerari/restaurant/Restaurant.java
@@ -0,0 +1,12 @@
+package itinerari.restaurant;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Restaurant {
+
+    
+
+}
