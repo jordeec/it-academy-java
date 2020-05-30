8104456fb6fbfbb1393449c4d6d2f6e1363d65fe
jordeec <61867781+jordeec@users.noreply.github.com>
2020-05-30 11:16:51 +0200

    Elminar alguns fitxer més de Visual Studio

--- 04-restaurant/js/restaurant-vaadim/frontend/js/fases.js
+++
@@ -1,2 +0,0 @@
-console.log("Al fases.js");
-
--- 04-restaurant/js/restaurant-vaadim/frontend/js/restaurant.js
+++
@@ -1,4 +0,0 @@
-function holaDesdeVaadin (parametre) {
-    console.log("Aquesta funció està al frontEnd JS i ha rebut el següent paràmetre!" + parametre);
-    
-}
