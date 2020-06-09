d807bb27da1137450b8ebd8e07e96ef2c016bae8
jordeec <61867781+jordeec@users.noreply.github.com>
2020-06-09 09:46:28 +0200

    Working REST controller without JPA

--- 06-videos/video/src/main/java/itinerari/jordic/video/service/VideoService.java
+++ 06-videos/video/src/main/java/itinerari/jordic/video/service/VideoService.java
@@ -1,5 +1,6 @@
 package itinerari.jordic.video.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -8,40 +9,50 @@ import itinerari.jordic.video.model.Video;
 
 @Service
 public class VideoService {
-    List<Video> videos = Arrays.asList(
-            new Video("1", "https://ftc.gov/quam/nec.jpg", "Metoprolol Succinate", "Bettie Page Reveals All"),
-            new Video("2","http://ezinearticles.com/parturient/montes/nascetur/ridiculus/mus/etiam.jsp",
-                    "Little Fevers Childrens Fever Pain Reliever", "In Darkness"),
-            new Video("3","http://ehow.com/leo/maecenas/pulvinar/lobortis/est/phasellus/sit.js",
-                    "SHISEIDO PERFECT REFINING FOUNDATION", "Lost Skeleton Returns Again"),
-            new Video("4","https://photobucket.com/in.xml", "Oxygen", "Army of Darkness"),
-            new Video("5","https://facebook.com/mauris/eget/massa/tempor/convallis/nulla/neque.png",
-                    "POPULUS TREMULOIDES POLLEN", "Starsuckers"),
-            new Video("6","https://e-recht24.de/sodales/sed/tincidunt/eu.png", "Venlafaxine Hydrochloride", "Player"),
-            new Video("7","https://oaic.gov.au/nisl/duis.jpg", "Potassium Chloride", "Sun Shines Bright"),
-            new Video("8","http://va.gov/massa/quis/augue/luctus.jsp", "ATORVASTATIN CALCIUM",
-                    "Asterix & Obelix vs. Caesar (Astérix et Obélix contre César)"),
-            new Video("9","https://nih.gov/nisl/venenatis/lacinia/aenean/sit/amet/justo.json",
-                    "Coppertone TattooGuard Sunscreen", "Hotel"),
-            new Video("10","http://macromedia.com/dapibus/at/diam/nam/tristique/tortor/eu.jsp", "BareMinerals",
-                    "Tokyo Decadence (Topâzu)"));
+        private List<Video> videos = new ArrayList<Video>(Arrays.asList(
+                        new Video("1", "https://ftc.gov/quam/nec.jpg", "Metoprolol Succinate",
+                                        "Bettie Page Reveals All"),
+                        new Video("2", "http://ezinearticles.com/parturient/montes/nascetur/ridiculus/mus/etiam.jsp",
+                                        "Little Fevers Childrens Fever Pain Reliever", "In Darkness"),
+                        new Video("3", "http://ehow.com/leo/maecenas/pulvinar/lobortis/est/phasellus/sit.js",
+                                        "SHISEIDO PERFECT REFINING FOUNDATION", "Lost Skeleton Returns Again"),
+                        new Video("4", "https://photobucket.com/in.xml", "Oxygen", "Army of Darkness"),
+                        new Video("5", "https://facebook.com/mauris/eget/massa/tempor/convallis/nulla/neque.png",
+                                        "POPULUS TREMULOIDES POLLEN", "Starsuckers"),
+                        new Video("6", "https://e-recht24.de/sodales/sed/tincidunt/eu.png", "Venlafaxine Hydrochloride",
+                                        "Player"),
+                        new Video("7", "https://oaic.gov.au/nisl/duis.jpg", "Potassium Chloride", "Sun Shines Bright"),
+                        new Video("8", "http://va.gov/massa/quis/augue/luctus.jsp", "ATORVASTATIN CALCIUM",
+                                        "Asterix & Obelix vs. Caesar (Astérix et Obélix contre César)"),
+                        new Video("9", "https://nih.gov/nisl/venenatis/lacinia/aenean/sit/amet/justo.json",
+                                        "Coppertone TattooGuard Sunscreen", "Hotel"),
+                        new Video("10", "http://macromedia.com/dapibus/at/diam/nam/tristique/tortor/eu.jsp",
+                                        "BareMinerals", "Tokyo Decadence (Topâzu)")));
 
-    public List<Video> getAllVideos() {
-        return videos;
-    }
+        public List<Video> getAllVideos() {
+                return videos;
+        }
 
-    public void addVideo(Video video) {
-        videos.add(video);
-    }
+        public void addVideo(Video video) {
+                videos.add(video);
+        }
 
-    public void deleteVideo(String id) {
-        videos.remove(getVideo(id));
-    }
+        public void deleteVideo(String id) {
+                videos.removeIf(t -> t.getId().equals(id));
+        }
 
-    public Video getVideo(String id) {
-        return videos.stream().filter(v -> v.getId().equals(id)).findFirst().get();
-	}
+        public Video getVideo(String id) {
+                return videos.stream().filter(v -> v.getId().equals(id)).findFirst().get();
+        }
 
-	public void updateVideo(String id, Video video) {
-	}
+        public void updateVideo(String id, Video video) {
+                int i = 0;
+                for (Video v : videos) {
+                        if (v.getId().equals(id)) {
+                                videos.set(i, video);
+                                break;
+                        }
+                        i++;
+                }
+        }
 }
