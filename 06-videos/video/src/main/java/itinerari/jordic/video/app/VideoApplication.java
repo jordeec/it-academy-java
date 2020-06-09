e69f7458801a61d86f74479f8d98ea50ec2df44f
jordeec <61867781+jordeec@users.noreply.github.com>
2020-06-09 07:22:15 +0200

    Moved /app to /video/app

---
+++ 06-videos/video/src/main/java/itinerari/jordic/video/app/VideoApplication.java
@@ -0,0 +1,13 @@
+package itinerari.jordic.video.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoApplication.class, args);
+	}
+
+}
